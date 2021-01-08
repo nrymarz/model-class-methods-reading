1948a60cd60248c1527bdfd680ffa1faf676deb8
nrymarz <nrymarz@gmail.com>
2021-01-08 00:22:05 -0600

    commit tree 372faad66a7504645ad53ed08e468e47a4103c13

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,17 @@ class Post < ActiveRecord::Base
   before_validation :make_title_case 
   belongs_to :author
 
-  #put new code here
+  def self.by_author(id)
+    Post.where(author_id: id)
+  end
+
+  def self.from_today
+    Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+
+  def self.old_news
+    Post.where("created_at <=?",Time.zone.today.beginning_of_day)
+  end
 
   private
 
@@ -17,4 +27,5 @@ class Post < ActiveRecord::Base
   def make_title_case
     self.title = self.title.titlecase
   end
+  
 end
